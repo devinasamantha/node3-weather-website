eff08574924afa26a2c2fdafb99139f3141325e0
Devina Samantha <samantha_devina@hotmail.com>
2020-10-27 10:02:56 +0800

    forecast updated

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,6 @@
 const request = require('request');
 
+
 const forecast = (latitude,longitude,callback) => {
     
     const url = 'http://api.weatherstack.com/current?access_key=8231a013548e1a4252e15f3040c16b29&query=' + latitude + ','+ longitude 
@@ -10,7 +11,9 @@ const forecast = (latitude,longitude,callback) => {
        } else if (body.error) {
            callback('Unable to find location.Try another search', undefined)
        } else {
-           callback( undefined, body.current.weather_descriptions[0] + '.It is currently ' + body.current.temperature +' degrees out.It feels like ' + body.current.feelslike + ' degrees out.')
+           callback( undefined, 
+            body.current.weather_descriptions[0] + '.It is currently ' + body.current.temperature +' degrees out.It feels like ' + body.current.feelslike + 
+            ' degrees out.The humidity is' + body.current.humidity + '%.')
        }
    })
 }
